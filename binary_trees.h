372d1835f973d08a5e5f88617248a426292bb291
Bacia22 <adolfobacia@yahoo.com>
2023-05-31 15:54:49 -0700

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,15 +1,16 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stdef.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 /**
- *struct binary_s - Binary tree node
+ * struct binary_tree_s - Binary tree node
  *
- * @n: integer store in the node
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
@@ -19,15 +20,15 @@ struct binary_tree_s
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
-	struct vianry_tree_s *right;
+	struct binary_tree_s *right;
 };
-typedef struct binaty_tree_s binary_tree_t;
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-/* linke list advanced 101 the levelorder */
+/*  linked list for advanced 101 the levelorder */
 /**
- * struct link_s - struct for advanced tasks
+ * struct link_s - structure for advanced tasks
  *
  * @n: depth of node specified
  * @node: node of tree to store
@@ -38,10 +39,9 @@ typedef struct link_s
 	size_t n;
 	struct binary_tree_s const *node;
 	struct link_s *next;
-}
-link_t;
+} link_t;
 
-/* ++++++++++++++mandatory task++++++++++*/
+/*  ----------------- mandatory task ---------------------------------*/
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -64,7 +64,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-/*+++++++++++++advacend tasks+++++++++++*/
+/*  ------------------ advanced task -----------------------------------*/
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
